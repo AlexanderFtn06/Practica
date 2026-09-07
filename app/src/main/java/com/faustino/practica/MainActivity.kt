5efaa6f1a6fba65c0e5fb7aa3bc06f96f3bc67b6
Alexander Faustino Quispe <faustinoalexander46@gmail.com>
2026-09-06 22:15:40 -0500

    Agregar boxs para aprender su formato

--- app/src/main/java/com/faustino/practica/MainActivity.kt
+++ app/src/main/java/com/faustino/practica/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -11,12 +12,18 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -27,6 +34,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -143,6 +151,10 @@ fun Combinacion(){
                 Text("tecsup")
             }
         }
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        MostrarCard()
     }
 }
 
@@ -197,6 +209,50 @@ fun  ContadorDos(){
 }
 
 
+//9, Los Card
+@Composable
+fun MostrarCard(){
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        )
+    ) {
+        Column(Modifier.padding(16.dp)) {
+            Text("Cita cofirmada", fontWeight = FontWeight.Bold)
+            Text("Martes 10:30 am - Consultorio 4")
+        }
+    }
+}
+
+//9, Box
+@Composable
+fun BoxExp(){
+    Box(
+        modifier = Modifier
+            .size(200.dp)
+            .background(Color.Red),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .size(140.dp)
+                .background(Color.Black),
+            contentAlignment = Alignment.Center
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(80.dp)
+                    .background(Color.Red),
+                contentAlignment = Alignment.Center
+
+            ){
+                Text("Hola")
+            }
+        }
+    }
+}
+
 
 
 
@@ -212,6 +268,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     PracticaTheme {
-
+        BoxExp()
     }
 }
